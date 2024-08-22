8e0cb41a446341a7eef11f3a87e6862183e3393d
antonmatviets <anton.matviets@homeflow.co.uk>
2024-08-22 16:31:59 +0300

    + form reworked + fields added + messages added

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,198 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Sign Up',
+      home: AuthorizationForm(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class AuthorizationForm extends StatefulWidget {
+  const AuthorizationForm({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _AuthorizationFormState createState() => _AuthorizationFormState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _AuthorizationFormState extends State<AuthorizationForm> {
+  final _formKey = GlobalKey<FormState>();
+  String _email = '';
+  String _password = '';
+  bool _submitted = false;
 
-  void _incrementCounter() {
+  Map<String, bool> _emailErrors = {
+    'empty': false,
+    'format': false,
+  };
+  Map<String, bool> _passwordErrors = {
+    'empty': false,
+    'lengthMin': false,
+    'lengthMax': false,
+    'uppercase': false,
+    'digit': false,
+  };
+
+  void _validateEmail(String value) {
+    _emailErrors['empty'] = value.isEmpty;
+    _emailErrors['format'] = !RegExp(r'^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value);
+  }
+
+  void _validatePassword(String value) {
+    _passwordErrors['empty'] = value.isEmpty;
+    _passwordErrors['lengthMin'] = value.length < 8;
+    _passwordErrors['lengthMax'] = value.length > 64;
+    _passwordErrors['uppercase'] = !RegExp(r'[A-Z]').hasMatch(value);
+    _passwordErrors['digit'] = !RegExp(r'[0-9]').hasMatch(value);
+  }
+
+  void _submitForm() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _submitted = true;
+      _validateEmail(_email);
+      _validatePassword(_password);
     });
+
+    if (_emailErrors.values.every((e) => !e) && _passwordErrors.values.every((e) => !e)) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Sign up'),
+            content: const Text('Sign up success'),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  Widget _buildErrorText(String message, bool hasError) {
+    return _submitted
+      ? Text(
+          message,
+          style: TextStyle(
+            color: hasError ? Colors.red : Colors.green,
+          ),
+        )
+      : const SizedBox.shrink();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Colors.blue,
+                Colors.red,
+              ],
+            )
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                      border: const OutlineInputBorder(),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: _submitted && _emailErrors.values.any((e) => e) ? Colors.red : Colors.grey,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: _submitted && _emailErrors.values.any((e) => e) ? Colors.red : Colors.grey,
+                        ),
+                      ),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    onChanged: (value) {
+                      setState(() {
+                        _email = value;
+                        if (_submitted) _validateEmail(value);
+                      });
+                    },
+                  ),
+                  const SizedBox(height: 5),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _buildErrorText('Email is required', _emailErrors['empty']!),
+                      _buildErrorText('Invalid email address', _emailErrors['format']!),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      border: const OutlineInputBorder(),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: _submitted && _passwordErrors.values.any((e) => e) ? Colors.red : Colors.grey,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: _submitted && _passwordErrors.values.any((e) => e) ? Colors.red : Colors.grey,
+                        ),
+                      ),
+                    ),
+                    obscureText: true,
+                    onChanged: (value) {
+                      setState(() {
+                        _password = value;
+                        if (_submitted) _validatePassword(value);
+                      });
+                    },
+                  ),
+                  const SizedBox(height: 5),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _buildErrorText('Password is required', _passwordErrors['empty']!),
+                      _buildErrorText('8 characters or more (no spaces)', _passwordErrors['lengthMin']!),
+                      _buildErrorText('No more than 64 characters', _passwordErrors['lengthMax']!),
+                      _buildErrorText('Uppercase and lowercase letters', _passwordErrors['uppercase']!),
+                      _buildErrorText('At least one digit', _passwordErrors['digit']!),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: _submitForm,
+                    child: const Text('Sign up'),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )
     );
   }
 }
